448871d019860ba7ac16a613fbe8c592854a1baf
afinit <mark.wilson.3121@gmail.com>
2023-06-11 11:30:25 -0500

    update WeatherServer to setup CORS

--- scala-backend/src/main/scala/com/funapps/weather/servers/WeatherServer.scala
+++ scala-backend/src/main/scala/com/funapps/weather/servers/WeatherServer.scala
@@ -3,9 +3,11 @@ package com.funapps.weather.servers
 import cats.effect.Async
 import com.comcast.ip4s._
 import com.funapps.weather.services.{OpenWeatherService, WeatherService}
+import org.http4s.Uri
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.middleware.Logger
+import org.http4s.headers.Origin
+import org.http4s.server.middleware.{CORS, Logger}
 
 object WeatherServer {
   def run[F[_] : Async](openWeatherAppId: String): F[Nothing] = {
@@ -19,8 +21,13 @@ object WeatherServer {
         WeatherRoutes.routes(weatherService, openWeatherService)
       ).orNotFound
 
+      httpAppWithCors = CORS.policy.withAllowOriginHost(Set(
+        Origin.Host(Uri.Scheme.http, Uri.RegName("localhost"), Some(3000))
+      ))
+        .apply(httpApp)
+
       // With Middlewares in place
-      finalHttpApp = Logger.httpApp(true, true)(httpApp)
+      finalHttpApp = Logger.httpApp(true, true)(httpAppWithCors)
 
       _ <-
         EmberServerBuilder.default[F]

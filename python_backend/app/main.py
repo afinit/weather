e85d052fd7164a65ea0ff003dca1341ab516aea1
afinit <mark.wilson.3121@gmail.com>
2023-06-22 13:17:20 -0500

    add cors support for fastapi backend

--- python_backend/app/main.py
+++ python_backend/app/main.py
@@ -3,11 +3,24 @@ from .routers import weather
 from .internal.service.weather_service import WeatherService
 import logging
 import sys
+from fastapi.middleware.cors import CORSMiddleware
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
                     format="%(asctime)s - %(levelname)s - %(message)s")
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(weather.router)
 
